9f0f981f6714732c010f06c13ec6e9b26348844a
Gully Foyle <mikanboy@gmail.com>
2011-05-21 23:43:23 +0000

    added H2Dialect.java, an empty class which extends PostgresSQLDialect

--- activejdbc/src/main/java/activejdbc/Configuration.java
+++ activejdbc/src/main/java/activejdbc/Configuration.java
@@ -20,6 +20,7 @@ package activejdbc;
 import activejdbc.cache.CacheManager;
 import activejdbc.cache.OSCacheManager;
 import activejdbc.dialects.DefaultDialect;
+import activejdbc.dialects.H2Dialect;
 import activejdbc.dialects.MySQLDialect;
 import activejdbc.dialects.OracleDialect;
 import activejdbc.dialects.PostgreSQLDialect;
@@ -111,7 +112,7 @@ public class Configuration {
                 dialects.put(mm.getDbType(), new PostgreSQLDialect());
             }
             else if(mm.getDbType().equalsIgnoreCase("h2")){
-                dialects.put(mm.getDbType(), new PostgreSQLDialect());
+                dialects.put(mm.getDbType(), new H2Dialect());
             }
             else{
                 dialects.put(mm.getDbType(), new DefaultDialect());
---
+++ activejdbc/src/main/java/activejdbc/dialects/H2Dialect.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package activejdbc.dialects;
+
+/**
+ * Supports features of the h2 sql dialect.
+ * 
+ * h2 database sql is very standard, and the developer seems to be adding
+ * Postgres and Mysql compatibility. 
+ * 
+ * @see http://www.h2database.com/html/grammar.html
+ * @author Phil Suh (http://filsa.net/)
+ */
+public class H2Dialect extends PostgreSQLDialect {
+
+}
