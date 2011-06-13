890293e39812c13bdfe5854a30b6276368a29427
Igor Polevoy <igor@polevoy.org>
2011-06-13 19:29:18 +0000

    added a spec to the Ant project, references: http://code.google.com/p/activejdbc/issues/detail?id=75

--- examples/ant-example/src/activejdbc/ant/example/Employee.java
+++ examples/ant-example/src/activejdbc/ant/example/Employee.java
@@ -2,4 +2,8 @@ package activejdbc.ant.example;
 
 import activejdbc.Model;
 
-public class Employee extends Model {}
+public class Employee extends Model {
+    static {
+        validatePresenceOf("first_name", "last_name");
+    }
+}
