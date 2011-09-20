7881965feef53808d1543014c6de53d6a18f5ef7
Igor Polevoy <igor@polevoy.org>
2011-09-20 19:54:07 +0000

    added repository

--- examples/kitchensink/src/main/java/app/config/FreeMarkerConfig.java
+++ examples/kitchensink/src/main/java/app/config/FreeMarkerConfig.java
@@ -22,7 +22,6 @@ import org.javalite.activeweb.freemarker.AbstractFreeMarkerConfig;
  * @author Igor Polevoy
  */
 public class FreeMarkerConfig extends AbstractFreeMarkerConfig {
-    //TODO: write test for custom tag registration
     public void init() {
         //this is to override a strange FreeMarker default processing of numbers 
         getConfiguration().setNumberFormat("0.##");
