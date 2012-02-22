a943f7afb4ab1295209ce92ad1e79319153e522a
Igor Polevoy <igor@polevoy.org>
2012-02-22 19:37:32 +0000

    Issue 139: 	Add convenience method to Http to convert map to content

---
+++ src/test/java/org/javalite/common/http/HttpSpec.java
@@ -0,0 +1,19 @@
+package org.javalite.common.http;
+
+import org.javalite.http.Http;
+import org.junit.Test;
+
+import static org.javalite.common.Collections.map;
+import static org.javalite.test.jspec.JSpec.the;
+
+/**
+ * @author Igor Polevoy: 2/22/12 1:29 PM
+ */
+public class HttpSpec {
+
+    @Test
+    public void shouldGenerateContentFromMap(){
+        String content = Http.map2Content(map("first_name", "John", "last_name", "Doe", "ssn", "123-45-6789"));
+        the(content).shouldEqual("first_name=John&ssn=123-45-6789&last_name=Doe");
+    }
+}
