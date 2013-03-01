a859f7ecd0aa916e4b70e13005ec5d28745c4887
Vasya Pupkin <vasyalike@gmail.com>
2013-03-01 08:30:54 +0000

    log query time in find(String sql, RowListener listener)

--- activejdbc/src/main/java/org/javalite/activejdbc/DB.java
+++ activejdbc/src/main/java/org/javalite/activejdbc/DB.java
@@ -420,10 +420,12 @@ public class DB {
         Statement s = null;
         ResultSet rs = null;
         try {
+            long start = System.currentTimeMillis();
             s = connection().createStatement();
             rs = s.executeQuery(sql);
             RowProcessor p = new RowProcessor(rs, s);
             p.with(listener);
+            LogFilter.logQuery(logger, sql, null, start);
         }
         catch (Exception e) {
             throw new DBException(sql, null, e);
