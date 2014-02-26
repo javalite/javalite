5993f573d8347a5ca2bba734b0a806b45531b89c
ipolevoy <ipolevoy@groupon.com>
2014-02-26 00:13:55 -0600

    activeweb-147: Move activeweb examples into separate repos

--- activeweb-bootstrap/src/main/java/app/controllers/HomeController.java
+++
@@ -1,7 +0,0 @@
-package app.controllers;
-
-import org.javalite.activeweb.AppController;
-
-public class HomeController extends AppController {
-    public void index(){}
-}
--- activeweb-bootstrap/src/main/webapp/WEB-INF/views/layouts/footer.ftl
+++
@@ -1,3 +0,0 @@
-<div class='footer'>
-    <p>2010 - 2012 Active Web. No Rights Reserved.</p>
-</div>
