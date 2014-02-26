785b0b6f01c242c655383611b7ed248a68773342
ipolevoy <ipolevoy@groupon.com>
2014-02-25 22:40:54 -0600

    activejdbc-211 Create separate repos for each example. Moved, deleting

--- standalone-example/run.bat
+++
@@ -1,9 +0,0 @@
-
-set CLASSPATH=classes
-set CLASSPATH=%CLASSPATH%;lib\activejdbc-1.4.1.jar
-set CLASSPATH=%CLASSPATH%;lib\javalite-common-1.4.1.jar
-set CLASSPATH=%CLASSPATH%;lib\mysql-connector-java-5.0.4.jar
-set CLASSPATH=%CLASSPATH%;lib\slf4j-api-1.5.10.jar
-set CLASSPATH=%CLASSPATH%;lib\slf4j-simple-1.5.10.jar
-
-java -classpath %CLASSPATH% -DoutputDirectory=$OUTDIR activejdbc.examples.simple.SimpleExample
--- standalone-example/run.sh
+++
@@ -1,8 +0,0 @@
-#
-# This script is an example of running instrumentation without Maven or Ant, just a simple command line.
-#
-
-export CLASSPATH=classes
-for file in `ls lib` ; do export  CLASSPATH=$CLASSPATH:lib/$file; done
-
-java -cp $CLASSPATH -DoutputDirectory=$OUTDIR activejdbc.examples.simple.SimpleExample
