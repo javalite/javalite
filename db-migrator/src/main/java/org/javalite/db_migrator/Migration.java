959ec50c443ce045c2d7eef304b3c528f25ccf39
15801697282_p <istarjingdong>
2014-07-01 00:34:34 +0800

    add .sql file encode check for command db:migrate

--- db-migrator/src/main/java/org/javalite/db_migrator/Migration.java
+++ db-migrator/src/main/java/org/javalite/db_migrator/Migration.java
@@ -29,12 +29,78 @@ public class Migration implements Comparable {
         return migrationFile.getName();
     }
 
+    private String getCharset(File file) {
+        String charset = "GBK"; // 默认编码
+        byte[] first3Bytes = new byte[3];
+        try {
+            boolean checked = false;
+            BufferedInputStream bis = new BufferedInputStream(
+                    new FileInputStream(file));
+            bis.mark(0);
+            int read = bis.read(first3Bytes, 0, 3);
+            if (read == -1)
+                return charset;
+            if (first3Bytes[0] == (byte) 0xFF && first3Bytes[1] == (byte) 0xFE) {
+                charset = "UTF-16LE";
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xFE && first3Bytes[1]
+                    == (byte) 0xFF) {
+                charset = "UTF-16BE";
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xEF && first3Bytes[1]
+                    == (byte) 0xBB
+                    && first3Bytes[2] == (byte) 0xBF) {
+                charset = "UTF-8";
+                checked = true;
+            }
+            bis.reset();
+            if (!checked) {
+                int loc = 0;
+                while ((read = bis.read()) != -1) {
+                    loc++;
+                    if (read >= 0xF0)
+                        break;
+                    //单独出现BF以下的，也算是GBK
+                    if (0x80 <= read && read <= 0xBF)
+                        break;
+                    if (0xC0 <= read && read <= 0xDF) {
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF)// 双字节 (0xC0 - 0xDF)
+                            // (0x80 -
+                            // 0xBF),也可能在GB编码内
+                            continue;
+                        else
+                            break;
+                        // 也有可能出错，但是几率较小
+                    } else if (0xE0 <= read && read <= 0xEF) {
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF) {
+                            read = bis.read();
+                            if (0x80 <= read && read <= 0xBF) {
+                                charset = "UTF-8";
+                                break;
+                            } else
+                                break;
+                        } else
+                            break;
+                    }
+                }
+                System.out.println(loc + " " + Integer.toHexString(read));
+            }
+            bis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return charset;
+    }
+
     public void migrate() throws Exception {
 
+        String encoding = getCharset(migrationFile);
         StringBuffer command = null;
         try {
             String delimiter = DEFAULT_DELIMITER;
-            LineNumberReader lineReader = new LineNumberReader(new FileReader(migrationFile));
+            LineNumberReader lineReader = new LineNumberReader(new InputStreamReader(new FileInputStream(migrationFile), encoding));
             String line;
             while ((line = lineReader.readLine()) != null) {
                 if (command == null) {
