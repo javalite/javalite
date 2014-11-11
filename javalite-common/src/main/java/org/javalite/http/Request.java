5220cf5ed61656455a4130778600b8487a580831
Igor Polevoy <igor@expresspigeon.com>
2014-11-11 16:21:19 -0600

    activejdbc #293 NPE in Http in case no error stream

--- javalite-common/src/main/java/org/javalite/http/Request.java
+++ javalite-common/src/main/java/org/javalite/http/Request.java
@@ -189,32 +189,25 @@ public abstract class Request<T extends Request> {
      * This will help keep-alive work smoothly.
      */
     public void dispose() {
-
         //according to this: http://download.oracle.com/javase/1.5.0/docs/guide/net/http-keepalive.html
         //should read all data from connection to make it happy.
-
         byte[] bytes = new byte[1024];
         try {
-            int count = 0;
             InputStream in = connection.getInputStream();
-            while ((count = in.read(bytes)) > 0) {
-            }//nothing
-
-            in.close();
+            if(in != null){
+                while ((in.read(bytes)) > 0) {}//do nothing
+                in.close();
+            }
         } catch (Exception ignore) {
             try {
                 InputStream errorStream = connection.getErrorStream();
-                int ret = 0;
-
-                while ((ret = errorStream.read(bytes)) > 0) {
-                }//nothing
-
-                errorStream.close();
-            } catch (IOException ignoreToo) {
-            }
+                if(errorStream != null){
+                    while ((errorStream.read(bytes)) > 0) {}//do nothing
+                    errorStream.close();
+                }
+            } catch (IOException ignoreToo) {}
         }
     }
-
 
     protected T connect() {
         if (!connected) {
