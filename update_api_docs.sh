51534b3793d936288ac327b598ace3cdf3570887
Igor Polevoy <igor@expresspigeon.com>
2014-11-17 13:23:24 -0600

    improved script

--- update_api_docs.sh
+++ update_api_docs.sh
@@ -1,8 +1,10 @@
 mvn javadoc:aggregate
 cp -rf target/site/apidocs/* ../javalite.github.io/activejdbc/
 
-echo "************************************************************************"
-echo "Do not forget to push changes in: ../javalite.github.io/activejdbc/"
-echo "************************************************************************"
 
-
+mvn javadoc:aggregate
+cp -rf target/site/apidocs/* ../javalite.github.io/activejdbc/
+cd ../javalite.github.io/activjdbc/
+git add .
+git commit -m "update JavaDoc"
+git push origin master
