2e2a316a59f7246620edff0b1b7d84511c62450f
Igor Polevoy <igor@expresspigeon.com>
2015-01-22 23:21:25 -0600

    #351 move app-config under activejdbc

---
+++ app-config/src/main/java/org/javalite/app_config/ConfigInitException.java
@@ -0,0 +1,7 @@
+package org.javalite.app_config;
+
+public class ConfigInitException extends RuntimeException {
+    public ConfigInitException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ app-config/src/main/java/org/javalite/app_config/Property.java
@@ -0,0 +1,57 @@
+package org.javalite.app_config;
+
+/**
+ * Identifies a configuration property. These properties are defined in environment - specific files, such as:
+ * <code>jdbc.prod.properties</code>.
+ *
+ * @author Igor Polevoy
+ */
+public class Property {
+    private String name;
+    private String value;
+    private String propertyFile;
+
+    /**
+     * @param name name of property.
+     * @param value value of property.
+     * @param propertyFile - path to property file this value is defined in.
+     */
+    public Property(String name, String value, String propertyFile) {
+        this.name = name;
+        this.value = value;
+        this.propertyFile = propertyFile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getPropertyFile() {
+        return propertyFile;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Property)){
+            return false;
+        }
+        Property other = (Property) obj;
+        return this.value.equals(other.value)
+                && this.propertyFile.equals(other.propertyFile)
+                && this.name.equals(other.name);
+    }
+}
