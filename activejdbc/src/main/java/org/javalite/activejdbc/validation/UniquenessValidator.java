999328600448a0489a473bab8f68c47501c9a6fd
Eric Nielsen <eric@amalgamar.com.br>
2015-02-26 16:07:31 -0300

    #234 Corrections for UniquenessValidator to work when model has custom database or id column name

--- activejdbc/src/main/java/org/javalite/activejdbc/validation/UniquenessValidator.java
+++ activejdbc/src/main/java/org/javalite/activejdbc/validation/UniquenessValidator.java
@@ -1,22 +1,23 @@
 /*
-Copyright 2009-2014 Igor Polevoy
+Copyright 2009-2015 Igor Polevoy
 
-Licensed under the Apache License, Version 2.0 (the "License"); 
-you may not use this file except in compliance with the License. 
-You may obtain a copy of the License at 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0 
+http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software 
-distributed under the License is distributed on an "AS IS" BASIS, 
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-See the License for the specific language governing permissions and 
-limitations under the License. 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 package org.javalite.activejdbc.validation;
 
-import org.javalite.activejdbc.Base;
+import org.javalite.activejdbc.DB;
+import org.javalite.activejdbc.MetaModel;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.Registry;
 
@@ -28,10 +29,12 @@ public class UniquenessValidator extends ValidatorAdapter {
         setMessage("should be unique");
     }
     @Override
-    public void validate(Model m) {
-        if (Base.count(Registry.instance().getMetaModel(m.getClass()).getTableName(),
-                attribute + " = ? AND id != ?", m.get(attribute), m.getId()) > 0) {
-            m.addValidator(this, attribute);
+    public void validate(Model model) {
+        MetaModel metaModel = Registry.instance().getMetaModel(model.getClass());
+        if (new DB(metaModel.getDbName()).count(metaModel.getTableName(),
+                attribute + " = ? AND " + metaModel.getIdName() + " != ?",
+                model.get(attribute), model.get(metaModel.getIdName())) > 0) {
+            model.addValidator(this, attribute);
         }
     }
 }
