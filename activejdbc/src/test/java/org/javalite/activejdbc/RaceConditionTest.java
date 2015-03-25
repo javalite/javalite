d78769bdbf5b10e4072bd19741bef0488c436482
Eric Nielsen <eric@amalgamar.com.br>
2015-03-25 15:08:13 -0300

    #381 Better handling of threads in RaceConditionTest

--- activejdbc/src/test/java/org/javalite/activejdbc/RaceConditionTest.java
+++ activejdbc/src/test/java/org/javalite/activejdbc/RaceConditionTest.java
@@ -4,39 +4,41 @@ import org.javalite.activejdbc.test.ActiveJDBCTest;
 import org.javalite.activejdbc.test_models.Person;
 import org.junit.Test;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Collections;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
 
 import static org.javalite.activejdbc.test.JdbcProperties.*;
 
 /**
  * @author Igor Polevoy: 4/4/12 2:40 PM
+ * @author Eric Nielsen
  */
 public class RaceConditionTest extends ActiveJDBCTest{
 
     @Test
     //TODO: what is test testing?
-    public void shouldNotGetRaceCondition() throws InterruptedException {
-
-        final ConcurrentLinkedQueue<Integer> queue = new ConcurrentLinkedQueue<Integer>();
-
-        Runnable r = new Runnable() {
-            @Override public void run() {
+    public void shouldNotGetRaceCondition() throws ExecutionException, InterruptedException {
+        Callable<Person> task = new Callable<Person>() {
+            @Override
+            public Person call() throws Exception {
                 Base.open(driver(), url(), user(), password());
                 Person p = new Person();
                 p.set("name", "Igor");
                 Base.close();
-                queue.add(1);
+                return p;
             }
         };
         ExecutorService executor = Executors.newFixedThreadPool(10);
-        for (int i = 0; i < 10; i++) {
-            executor.execute(r);
+        try {
+            for (Future<Person> future : executor.invokeAll(Collections.nCopies(10, task))) {
+                future.get();
+            }
+        } finally {
+            executor.shutdownNow();
         }
-        executor.shutdown();
-        executor.awaitTermination(1, TimeUnit.MINUTES);
-        the(queue.size()).shouldEqual(10);
     }
 }
