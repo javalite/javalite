532331b5345f3da779747a8a4d9db7c41d2c8cd6
Igor Polevoy <igor@expresspigeon.com>
2015-04-07 12:32:38 -0500

    typo

--- update_api_docs.sh
+++ update_api_docs.sh
@@ -2,7 +2,7 @@
 
 mvn javadoc:aggregate
 cp -rf target/site/apidocs/* ../javalite.github.io/activejdbc/
-cd ../javalite.github.io/activjdbc/
+cd ../javalite.github.io/activejdbc/
 git add .
 git commit -m "update JavaDoc"
 git push origin master
