2a12d71ddc597d72ca3f81ccd1b766033615ad50
Jason Maher <jason@jasonmaher.me>
2015-11-18 13:09:23 +0900

    Create a sub-project that uses the plugin for testing. Reproduces #438

---
+++ activejdbc-gradle-plugin/test-project/src/main/resources/db/migration/V1__Create_example_table.sql
@@ -0,0 +1,4 @@
+create table EXAMPLES (
+  ID int not null auto_increment,
+  NAME text
+);
---
+++ activejdbc-gradle-plugin/test-project/src/test/java/org/javalite/activejdbc/gradle/plugin/test/ExampleTest.java
@@ -0,0 +1,27 @@
+package org.javalite.activejdbc.gradle.plugin.test;
+
+import org.javalite.activejdbc.Base;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.SafeVarargs;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testOk() {
+        Base.open("org.h2.Driver", "jdbc:h2:file:./build/testdb", null, null);
+        for (String name : Arrays.asList("foo", "bar", "baz")) {
+            Example e = new Example();
+            e.set("name", name);
+            e.saveIt();
+        }
+        assertEquals(3, Example.findAll().size());
+        Base.close();
+    }
+
+}
