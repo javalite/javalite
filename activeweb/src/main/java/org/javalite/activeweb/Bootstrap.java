26711aad9b5f328f4b2a400bbf95777ae4a9d3de
Igor Polevoy <igor@expresspigeon.com>
2015-11-29 11:00:33 -0600

    added JavaDoc

--- activeweb/src/main/java/org/javalite/activeweb/Bootstrap.java
+++ activeweb/src/main/java/org/javalite/activeweb/Bootstrap.java
@@ -74,11 +74,20 @@ public abstract class Bootstrap extends AppConfig{
     /**
      * Subclasses need to override this method to return instance of Injector to use for dependency injection.
      *
-     * <p></p>
-
      * <strong>
+     *     <p>
      *     This method is NOT USED during testing. Each test class will set its own injector
      *     with mocks for testing.
+     *     </p>
+     *     <p>
+     *     It is important to not create a new instance of injector each time this method is called, but
+     *     rather than return the same instance.
+     *     </p>
+     *     <p>
+     *     No synchronization is needed because the first time
+     *     this method is used, it is done in one thread.
+     *     </p>
+     *
      * </strong>
      *
      * @return instance of Injector to use to inject dependencies into controllers, filters and tags.
