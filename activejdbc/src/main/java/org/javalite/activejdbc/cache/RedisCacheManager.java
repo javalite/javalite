6b2b1cafe8ecb36d4c3a4a83b6b108b21c5e2085
Igor Polevoy <igor@expresspigeon.com>
2015-12-09 12:37:08 -0600

    #446 Implement cache manager based on Redis

--- activejdbc/src/main/java/org/javalite/activejdbc/cache/RedisCacheManager.java
+++ activejdbc/src/main/java/org/javalite/activejdbc/cache/RedisCacheManager.java
@@ -1,76 +1,49 @@
 package org.javalite.activejdbc.cache;
 
 
-/**
- * Jedis jedis = new Jedis("localhost");
- * <p/>
- * ByteArrayOutputStream bout = new ByteArrayOutputStream();
- * <p/>
- * ObjectOutput objectOutput = new ObjectOutputStream(bout);
- * <p/>
- * <p/>
- * objectOutput.writeObject(list);
- * objectOutput.close();
- * byte[] bytes = bout.toByteArray();
- * jedis.set("foo".getBytes(), bytes);
- * bytes = jedis.get("foo".getBytes());
- * <p/>
- * ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bytes));
- * Object o = in.readObject();
- * System.out.println(o);
- */
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.BinaryJedis;
 import redis.clients.jedis.Jedis;
 
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
-import java.util.Arrays;
 
 /**
  * @author Igor Polevoy on 12/7/15.
  */
 public class RedisCacheManager extends CacheManager {
-    Jedis jedis = new Jedis("localhost");
+    private static final Logger logger = LoggerFactory.getLogger(RedisCacheManager.class);
 
+    private BinaryJedis jedis = new Jedis("localhost");
 
     @Override
     public Object getCache(String group, String key) {
-        byte[] groupKey = concat(group.getBytes(), key.getBytes());
-        jedis.del(groupKey);
-        return null;
+        return jedis.hget(group.getBytes(), key.getBytes());
     }
 
     @Override
     public void addCache(String group, String key, Object cache) {
-
         try {
             ByteArrayOutputStream bout = new ByteArrayOutputStream();
             ObjectOutput objectOutput = new ObjectOutputStream(bout);
             objectOutput.writeObject(cache);
             objectOutput.close();
             byte[] bytes = bout.toByteArray();
-            byte[] groupKey = concat(group.getBytes(), key.getBytes());
-            //jedis.zadd()set(groupKey, bytes, "NX".getBytes(), "EX".getBytes(), 100000L);
+            jedis.hset(group.getBytes(), key.getBytes(), bytes);
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Failed to add object to cache with group: " + group + " and key: " + key, e);
         }
-
-    }
-
-    private byte[] concat(byte[] a, byte[] b) {
-        int aLen = a.length;
-        int bLen = b.length;
-        byte[] c = new byte[aLen + bLen];
-        System.arraycopy(a, 0, c, 0, aLen);
-        System.arraycopy(b, 0, c, aLen, bLen);
-        return c;
     }
 
     @Override
     public void doFlush(CacheEvent event) {
 
-        event.getSource();
-
+        if(event.getType().equals(CacheEvent.CacheEventType.ALL)){
+            throw new UnsupportedOperationException("Flushing all caches not supported");
+        }else if (event.getType().equals(CacheEvent.CacheEventType.GROUP)) {
+            jedis.hdel(event.getGroup().getBytes());
+        }
     }
 }
