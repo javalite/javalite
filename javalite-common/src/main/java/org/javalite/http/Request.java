31d74eb737c45deabb6a818b6c9cf19b7645d11d
Igor Polevoy <igor@expresspigeon.com>
2016-01-29 23:33:30 -0600

    #467 Improve code closing resource in Http Request class

--- javalite-common/src/main/java/org/javalite/http/Request.java
+++ javalite-common/src/main/java/org/javalite/http/Request.java
@@ -191,18 +191,14 @@ public abstract class Request<T extends Request> {
         //according to this: http://download.oracle.com/javase/1.5.0/docs/guide/net/http-keepalive.html
         //should read all data from connection to make it happy.
         byte[] bytes = new byte[1024];
-        try {
-            InputStream in = connection.getInputStream();
+        try (InputStream in = connection.getInputStream()){
             if(in != null){
                 while ((in.read(bytes)) > 0) {}//do nothing
-                in.close();
             }
         } catch (Exception ignore) {
-            try {
-                InputStream errorStream = connection.getErrorStream();
+            try(InputStream errorStream = connection.getErrorStream()) {
                 if(errorStream != null){
                     while ((errorStream.read(bytes)) > 0) {}//do nothing
-                    errorStream.close();
                 }
             } catch (IOException ignoreToo) {}
         }
