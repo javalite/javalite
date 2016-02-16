0946e90f186aef5ce4f786e7407f39ed2302f2b5
Igor Polevoy <igor@expresspigeon.com>
2016-02-16 12:52:50 -0600

    #271 Implement statistics gathering filter

---
+++ activeweb/src/main/java/org/javalite/activeweb/controller_filters/StatisticsFilter.java
@@ -0,0 +1,27 @@
+package org.javalite.activeweb.controller_filters;
+
+import org.javalite.activejdbc.Registry;
+import org.javalite.activejdbc.statistics.QueryExecutionEvent;
+
+/**
+ * This filter will collect statistics about controller/action execution into
+ * <a href="http://javalite.io/statistics_reporting">Statistics Queue</a> for analysis.
+ *
+ *
+ * @author Igor Polevoy on 2/16/16.
+ */
+public class StatisticsFilter extends HttpSupportFilter{
+
+    private static ThreadLocal<Long> start;
+
+    @Override
+    public void before() {
+        start.set(System.currentTimeMillis());
+    }
+
+    @Override
+    public void after() {
+        Registry.instance().getStatisticsQueue().enqueue(
+                new QueryExecutionEvent(getRoute().getController() + "#" + getRoute().getActionName(), System.currentTimeMillis() - start.get()));
+    }
+}
