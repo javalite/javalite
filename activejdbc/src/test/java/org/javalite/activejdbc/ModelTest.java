330128c83f336c6dea38fcf44dc5cb5c7310ca66
Igor Polevoy <igor@expresspigeon.com>
2016-04-23 23:09:47 -0500

    debug logging

--- activejdbc/src/test/java/org/javalite/activejdbc/ModelTest.java
+++ activejdbc/src/test/java/org/javalite/activejdbc/ModelTest.java
@@ -517,6 +517,7 @@ public class ModelTest extends ActiveJDBCTest {
         the(Base.exec(updateSql)).shouldBeEqual(1);
 
         alarm = Alarm.findById(alarm.getId());
+        System.out.println("ALARM: " + alarm.get("alarm_time").getClass() + " " + alarm.get("alarm_time"));
         the(alarm.getTime("alarm_time").toString()).shouldBeEqual(t);
     }
 
