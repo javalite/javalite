13337273fe75e02f88ab2ea819b7f3542290ff63
Igor Polevoy <igor@expresspigeon.com>
2016-12-12 23:49:53 -0600

    #561 Implement configuration of RedisCacheManager with AppConfig

--- activejdbc/src/main/java/org/javalite/activejdbc/cache/RedisCacheManager.java
+++ activejdbc/src/main/java/org/javalite/activejdbc/cache/RedisCacheManager.java
@@ -1,27 +1,47 @@
 package org.javalite.activejdbc.cache;
 
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.javalite.activejdbc.InitException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.io.*;
 import java.util.Properties;
 
+import static org.javalite.app_config.AppConfig.p;
 import static org.javalite.common.Convert.toInteger;
+import static org.javalite.common.Util.blank;
 
 /**
  * Redis cache manager. Will store caches in Redis server. By default will connect to Redis running on
  * local host.
  * <p>
- * If redis server is located elsewhere, provide a property file called <code>activejdbc-redis.properties</code>
- * with two properties: <code>redist-host</code> and <code>redist-port</code>.
- * </p>
- * <p>
- * The properties file needs to be at the root of classpath.
- * </p>
+ * If redis server is located elsewhere, provide property files  for different environments according to
+ * <code>AppConfig</code> rules.
+ * See: <a href="http://javalite.github.io/activejdbc/snapshot/org/javalite/app_config/AppConfig.html">AppConfig</a>
+ * for more information.
+ *
+ * The environment-specific properties files have to have the following properties for this class to fuction:
+ *
+ * <ul>
+ *     <li>
+ *         <code>redis.cache.manager.host</code>
+ *
+ *     </li>
+ *     <li>
+ *         <code>redis.cache.manager.port</code>
+ *     </li>
+ * </ul>
+ *
+ * <p></p>
+ *
+ * If the properties or property files are missing, this class will default to <code>localhost</code> and default
+ * port for Redis.
+ *
  * <p><strong>Limitation:</strong> Does not support {@link #flush(CacheEvent)} with value 'ALL'.</p>
  *
  * @author Igor Polevoy on 12/7/15.
@@ -33,19 +53,12 @@ public class RedisCacheManager extends CacheManager {
 
 
     public RedisCacheManager() {
-        Properties props = new Properties();
-        InputStream in = getClass().getResourceAsStream("/activejdbc-redis.properties");
-        if (in == null) {
-            jedisPool = new JedisPool("localhost");
-        } else {
-            try {
-                props.load(in);
-                String host = props.getProperty("redis-host");
-                String port = props.getProperty("redis-port");
-                jedisPool = new JedisPool(host, toInteger(port));
-            } catch (Exception e) {
-                throw new InitException("Failed to configure connection to Redis server", e);
-            }
+        try {
+            String host = p("redis.cache.manager.host");
+            String port = p("redis.cache.manager.port");
+            jedisPool = blank(host) || blank(port) ? new JedisPool() : new JedisPool(host,toInteger(port));
+        } catch (Exception e) {
+            throw new InitException("Failed to configure connection to Redis server", e);
         }
     }
 
