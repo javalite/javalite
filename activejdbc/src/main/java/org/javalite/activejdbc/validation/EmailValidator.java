3b8d072e58aa10f92a5b9572fb27ca4fe0a30ed7
Igor Polevoy <igor@expresspigeon.com>
2018-02-22 16:42:07 -0600

    #620 EmailValidator fails on legal emails

--- activejdbc/src/main/java/org/javalite/activejdbc/validation/EmailValidator.java
+++ activejdbc/src/main/java/org/javalite/activejdbc/validation/EmailValidator.java
@@ -19,7 +19,7 @@ package org.javalite.activejdbc.validation;
 
 public class EmailValidator extends RegexpValidator {
     public EmailValidator(String attribute){
-        super(attribute, "\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
+        super(attribute, "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
         setMessage("email has bad format");
     }
 }
