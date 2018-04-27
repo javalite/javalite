b1d9161bc04519a447d28f0617e9ce617821566c
ipolevoy <igor@expresspigeon.com>
2018-04-27 15:43:43 -0500

    #710 Waiting for external process in RuntimeUtil sometimes blocks indefinitely

--- javalite-common/src/main/java/org/javalite/common/RuntimeUtil.java
+++ javalite-common/src/main/java/org/javalite/common/RuntimeUtil.java
@@ -1,6 +1,9 @@
 package org.javalite.common;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 import static org.javalite.common.Util.read;
 
@@ -11,7 +14,7 @@ import static org.javalite.common.Util.read;
  */
 public class RuntimeUtil {
     /**
-     * Executes an external command and provides results of execution.
+     * Executes an external command and provides results of execution. Will accummulate limited output from the external process.
      *
      * @param command array containing the command to call and its arguments.
      *
@@ -20,10 +23,22 @@ public class RuntimeUtil {
     public static Response execute(String ... command) {
         try {
             Process process = Runtime.getRuntime().exec(command);
-            String out = read(process.getInputStream());
-            String err = read(process.getErrorStream());
 
-            return new Response(out, err, process.waitFor());
+            OutputReader stdOutReader = new OutputReader(process.getInputStream());
+            OutputReader stdErrReader = new OutputReader(process.getErrorStream());
+
+            Thread t1 = new Thread(stdOutReader);
+            t1.start();
+            Thread t2 = new Thread(stdErrReader);
+            t2.start();
+            int code = process.waitFor();
+            t1.join();
+            t2.join();
+
+            String out = stdOutReader.getOutput();
+            String err = stdErrReader.getOutput();
+
+            return new Response(out, err, code);
         } catch (InterruptedException ie) {
             Thread.currentThread().interrupt();
             throw  new RuntimeException("Interrupted");
@@ -58,7 +73,47 @@ public class RuntimeUtil {
         return execute(Util.split(command, " "));
     }
 
+    /**
+     * Will accumulate tail of STDIO or STDERR from the process in a separate thread
+     */
+    static class OutputReader implements Runnable {
+
+        private static final int MAX_SIZE = 2048;
+        private InputStream is;
+        private  LinkedList<Character> buffer = new LinkedList<>();
+
+        OutputReader(InputStream is) {
+            this.is = is;
+        }
+        public void run() {
+            try {
+                InputStreamReader reader = new InputStreamReader(is);
+                int s;
+                while ((s = reader.read()) != -1) {
+                    if(buffer.size() == MAX_SIZE){
+                        buffer.remove(0);
+                    }
+                    buffer.add((char)s); // boxing
+                }
+                is.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        public String getOutput() {
+
+            StringBuilder stringBuilder = new StringBuilder(buffer.size());
+            for (Character character : buffer) {
+                stringBuilder.append(character);
+            }
+            return stringBuilder.toString();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(execute("ls -ls").out);
+
+        System.out.println(execute("ls", "-ls").out);
     }
 }
