2b08781eb9a84f3bbd577c745d8273e4f86d6caa
João Francisco Almeida <joao267@gmail.com>
2018-08-07 23:43:06 +0100

    TravisCI - Attempt to setup deployment (without db2/postgres)

--- .travisci/setup.sh
+++ .travisci/setup.sh
@@ -5,3 +5,6 @@ set -e # Exit with nonzero exit code if anything fails
 
 # Reduce Maven download log
 export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+
+# Replace maven settings.xml file
+cp -f .travisci/settings.xml ~/.m2/
