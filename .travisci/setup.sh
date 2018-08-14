126ffbccb22a1e8dda532dd658471b82e2dce681
ipolevoy <igor@expresspigeon.com>
2018-08-14 09:27:43 -0500

    trying to get the upload log lines to show

--- .travisci/setup.sh
+++ .travisci/setup.sh
@@ -4,7 +4,7 @@
 set -e # Exit with nonzero exit code if anything fails
 
 # Reduce Maven download log
-export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+#export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 
 # Replace maven settings.xml file
 cp -f .travisci/settings.xml ~/.m2/
