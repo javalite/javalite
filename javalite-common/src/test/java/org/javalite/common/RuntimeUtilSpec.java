15f7cc2fd887b682f7a416fd8c577e1de03422c0
cschabli <christof@schablinski.de>
2018-10-19 09:28:38 +0200

    #798: skip RuntimeUtilSpec on Windows

--- javalite-common/src/test/java/org/javalite/common/RuntimeUtilSpec.java
+++ javalite-common/src/test/java/org/javalite/common/RuntimeUtilSpec.java
@@ -1,9 +1,11 @@
 package org.javalite.common;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.Test;
 
 import static org.javalite.common.RuntimeUtil.execute;
 import static org.javalite.test.jspec.JSpec.the;
+import static org.junit.Assume.assumeFalse;
 
 /**
  * @author igor on 9/23/18.
@@ -12,21 +14,29 @@ public class RuntimeUtilSpec {
 
     @Test
     public void shouldUseDefaultBufferSize(){
+        assumeFalse(SystemUtils.IS_OS_WINDOWS);
+
         the(execute("ls -ls").out).shouldContain("pom.xml");
     }
 
     @Test
     public void shouldUseDefaultBufferSizeSplitArguments(){
+        assumeFalse(SystemUtils.IS_OS_WINDOWS);
+
         the(execute("ls", "-ls").out).shouldContain("pom.xml");
     }
 
     @Test
     public void shouldOverrideBufferSize(){
+        assumeFalse(SystemUtils.IS_OS_WINDOWS);
+
         the(execute(4096, "ls", "-ls").out).shouldContain("pom.xml");
     }
 
     @Test
     public void shouldOverrideBufferSizeSplitArguments(){
+        assumeFalse(SystemUtils.IS_OS_WINDOWS);
+
         the(execute(4096, "ls -ls").out).shouldContain("pom.xml");
     }
 }
