0396172b37e6562434cab1951e1ac2bc317e7eb7
ipolevoy <igor@expresspigeon.com>
2019-06-28 15:27:17 -0500

    script to release JavaDoc

---
+++ scripts/release_javadoc.sh
@@ -0,0 +1,13 @@
+   VERSION=$1
+
+   mvn javadoc:aggregate
+
+   mkdir  ../javalite.github.io/activeweb/$VERSION
+
+   cp -rf target/site/apidocs/* ../javalite.github.io/activeweb/$VERSION
+   cd ../javalite.github.io/
+   git add .
+   git commit -m "added version $VERSION"
+
+   echo "Done creating the JavaDoc for $VERSION. Please, execute git push if you are happy"
+   git push
