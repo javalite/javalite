aabc92eea99dae469196c7e1c9e5d0b01481e203
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2019-09-30 15:59:45 -0500

    #882 Implement ability to set fetch size and other params for streaming result set

--- activejdbc/src/main/java/org/javalite/activejdbc/RowProcessor.java
+++ activejdbc/src/main/java/org/javalite/activejdbc/RowProcessor.java
@@ -25,6 +25,39 @@ import static org.javalite.common.Util.*;
 
 
 public class RowProcessor {
+
+    public enum ResultSetType{
+        FORWARD_ONLY(ResultSet.TYPE_FORWARD_ONLY),
+        SCROLL_INSENSITIVE(ResultSet.TYPE_SCROLL_INSENSITIVE),
+        SCROLL_SENSITIVE(ResultSet.TYPE_SCROLL_SENSITIVE);
+
+        private int value;
+
+        ResultSetType(int value){
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public enum ResultSetConcur{
+
+        READ_ONLY(ResultSet.CONCUR_READ_ONLY),
+        UPDATABLE(ResultSet.CONCUR_UPDATABLE);
+
+        private int value;
+
+        ResultSetConcur(int value){
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
     private final ResultSet rs;
     private final Statement s;
 
