83efde82821372f538f03efce3c76f3e3404c2a5
mppfiles <mppfiles@users.noreply.github.com>
2019-12-05 09:15:26 -0300

    Update BelongsTo.java
    Sorry! forgot the import...

--- activejdbc/src/main/java/org/javalite/activejdbc/annotations/BelongsTo.java
+++ activejdbc/src/main/java/org/javalite/activejdbc/annotations/BelongsTo.java
@@ -21,6 +21,7 @@ import org.javalite.activejdbc.Model;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
