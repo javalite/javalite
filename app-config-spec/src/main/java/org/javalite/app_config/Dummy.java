9c6d54b693770b41d3c50c579d3f4cec10e86d7b
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2020-10-05 11:20:31 -0500

    adding a dummy class to make Maven happy (see comments in  class)

---
+++ app-config-spec/src/main/java/org/javalite/app_config/Dummy.java
@@ -0,0 +1,10 @@
+package org.javalite.app_config;
+
+/**
+ * Exists to make this error go away:
+ * [ERROR] Failed to execute goal org.apache.maven.plugins:maven-javadoc-plugin:3.1.1:jar (attach-javadocs) on project app-config-spec: MavenReportException: Error while generating Javadoc:
+ */
+public class Dummy {
+
+
+}
