6f762ce9c39230b0203aa6ed7c8eb32f878470d6
yanchevsky <>
2021-08-25 19:47:04 +0300

    #1104 - Add ability to provide variables in migrations

--- db-migrator-maven-plugin/src/test/java/org/javalite/db_migrator/MergePropertiesSpec.java
+++ db-migrator-maven-plugin/src/test/java/org/javalite/db_migrator/MergePropertiesSpec.java
@@ -2,13 +2,13 @@ package org.javalite.db_migrator;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.javalite.db_migrator.maven.MigrateMojo;
-import org.javalite.json.JSONHelper;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import static org.javalite.common.Collections.map;
 import static org.javalite.db_migrator.JdbcPropertiesOverride.*;
 import static org.javalite.test.jspec.JSpec.the;
 
@@ -17,63 +17,54 @@ public class MergePropertiesSpec {
     @Test
     public void shouldUseEmptyMergePropertiesWithDefaultEnvironment() throws MojoExecutionException {
         Map<String, Properties> envProperties = execute(null, null);
-        the(compareMap(JSONHelper.toMap("{\n" +
-                "    \"driver\" : \"" + driver() + "\",\n" +
-                "    \"password\" : \"" + password() + "\",\n" +
-                "    \"url\" : \"" + fullUrl() + "\",\n" +
-                "    \"username\" : \"" + user() + "\"\n" +
-                "}"), envProperties.get("null"))).shouldBeTrue();
+        the(envProperties.get("null")).shouldBeEqual(map("driver", driver(), "password", password(), "url", fullUrl(), "username", user()));
     }
 
     @Test
     public void shouldUseMergePropertiesWithDefaultEnvironment() throws MojoExecutionException {
         Map<String, Properties> envProperties = execute(null,"/test_migrations/templator/table-names.properties");
-        the(compareMap(JSONHelper.toMap("{\n" +
-                "    \"authors-table\" : \"authors\",\n" +
-                "    \"books-table\" : \"books\",\n" +
-                "    \"driver\" : \"" + driver() + "\",\n" +
-                "    \"development.authors-table\" : \"development-authors\",\n" +
-                "    \"development.books-table\" : \"development-books\",\n" +
-                "    \"staging.books-table\" : \"staging-books\",\n" +
-                "    \"password\" : \"" + password() + "\",\n" +
-                "    \"url\" : \"" + fullUrl() + "\",\n" +
-                "    \"username\" : \"" + user() + "\"\n" +
-                "}"), envProperties.get("null"))).shouldBeTrue();
+        the(envProperties.get("null")).shouldBeEqual(map("authors-table", "authors",
+                "books-table", "books",
+                "driver", driver(),
+                "development.authors-table", "development-authors",
+                "development.books-table", "development-books",
+                "staging.books-table", "staging-books",
+                "password", password(),
+                "url", fullUrl(),
+                "username", user()));
     }
 
     @Test
     public void shouldUseMergePropertiesWithDevelopmentEnvironment() throws MojoExecutionException {
         Map<String, Properties> envProperties = execute("development","/test_migrations/templator/table-names.properties");
-        the(compareMap(JSONHelper.toMap("{\n" +
-                "    \"authors-table\" : \"development-authors\",\n" +
-                "    \"books-table\" : \"development-books\",\n" +
-                "    \"driver\" : \"" + driver() + "\",\n" +
-                "    \"staging.books-table\" : \"staging-books\",\n" +
-                "    \"password\" : \"" + password() + "\",\n" +
-                "    \"url\" : \"" + fullUrl() + "\",\n" +
-                "    \"username\" : \"" + user() + "\"\n" +
-                "}"), envProperties.get("development"))).shouldBeTrue();
+        the(envProperties.get("development")).shouldBeEqual(map(
+                "authors-table", "development-authors",
+                "books-table", "development-books",
+                "driver", driver(),
+                "staging.books-table", "staging-books",
+                "password", password(),
+                "url", fullUrl(),
+                "username", user()
+        ));
     }
 
     @Test
     public void shouldUseMergePropertiesWithDevelopmentAndStagingEnvironments() throws MojoExecutionException {
         Map<String, Properties> envProperties = execute("development,staging","/test_migrations/templator/table-names.properties");
-        the(compareMap(JSONHelper.toMap("{\n" +
-                "    \"authors-table\" : \"development-authors\",\n" +
-                "    \"books-table\" : \"development-books\",\n" +
-                "    \"driver\" : \"" + driver() + "\",\n" +
-                "    \"password\" : \"" + password() + "\",\n" +
-                "    \"url\" : \"" + fullUrl() + "\",\n" +
-                "    \"username\" : \"" + user() + "\"\n" +
-                "}"), envProperties.get("development"))).shouldBeTrue();
-        the(compareMap(JSONHelper.toMap("{\n" +
-                "    \"authors-table\" : \"authors\",\n" +
-                "    \"books-table\" : \"staging-books\",\n" +
-                "    \"driver\" : \"" + driver() + "\",\n" +
-                "    \"password\" : \"" + password() + "\",\n" +
-                "    \"url\" : \"jdbc:mysql://localhost/test_project_stage\",\n" +
-                "    \"username\" : \"" + user() + "\"\n" +
-                "}"), envProperties.get("staging"))).shouldBeTrue();
+        the(envProperties.get("development")).shouldBeEqual(map(
+                "authors-table", "development-authors",
+                "books-table", "development-books",
+                "driver", driver(),
+                "password", password(),
+                "url", fullUrl(),
+                "username", user()));
+        the(envProperties.get("staging")).shouldBeEqual(map(
+                "authors-table", "authors",
+                "books-table", "staging-books",
+                "driver", driver(),
+                "password", password(),
+                "url", "jdbc:mysql://localhost/test_project_stage",
+                "username", user()));
     }
 
     private Map<String, Properties> execute(String environments, String mergeProperties) throws MojoExecutionException {
@@ -95,32 +86,5 @@ public class MergePropertiesSpec {
         migrateMojo.execute();
         return result;
     }
-
-    private boolean compareMap(Map m1, Map m2) {
-        System.out.println("-----------");
-        System.out.println(JSONHelper.toJsonString(m1, true));
-        System.out.println("              VS");
-        System.out.println(JSONHelper.toJsonString(m1, true));
-        if (m1 == m2) return true;
-        if (m1 == null || m2 == null) {
-            System.out.println(m1 + " != " + m2);
-            return false;
-        }
-        if (m1.size() != m2.size()) {
-            System.out.println(m1.size() + " != " + m2.size());
-            return false;
-        }
-        for (Object o : m1.entrySet()) {
-            Map.Entry entry = (Map.Entry) o;
-            Object v = m2.get(entry.getKey());
-            if (!entry.getValue().equals(v)) {
-                System.out.println(entry.getKey() + "=" + entry.getValue() + " != " + entry.getKey() + "=" + v);
-                return false;
-            }
-        }
-        return true;
-    }
-
-
 
 }
