a7ed0134d9e9de74822022e51c4c3c172ac02de6
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2021-11-22 12:16:21 -0600

    #1160 - Implement handling concurrent calls in getModelRegistry

--- activejdbc/src/main/java/org/javalite/activejdbc/MetaModels.java
+++ activejdbc/src/main/java/org/javalite/activejdbc/MetaModels.java
@@ -27,6 +27,7 @@ import org.slf4j.LoggerFactory;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static java.util.Collections.emptyList;
 import static org.javalite.common.Collections.map;
@@ -51,7 +52,7 @@ class MetaModels {
     private final Map<String, MetaModel> metaModelsByClassName = new HashMap<>();
     //these are all many to many associations across all models.
     private final List<Many2ManyAssociation> many2ManyAssociations = new ArrayList<>();
-    private final Map<Class, ModelRegistry> modelRegistries = new HashMap<>();
+    private final Map<Class, ModelRegistry> modelRegistries = new ConcurrentHashMap<>();
 
     void addMetaModel(MetaModel mm, Class<? extends Model> modelClass) {
         Object o = metaModelsByClassName.put(modelClass.getName(), mm);
