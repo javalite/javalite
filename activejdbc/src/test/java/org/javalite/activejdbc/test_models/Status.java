39f85106086b382bba75054feff6b4bbf215ae55
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2022-01-04 16:33:45 -0600

    #640 - Handling postgres types - cleanup

--- activejdbc/src/test/java/org/javalite/activejdbc/test_models/Status.java
+++ activejdbc/src/test/java/org/javalite/activejdbc/test_models/Status.java
@@ -1,7 +1,5 @@
 package org.javalite.activejdbc.test_models;
 
 import org.javalite.activejdbc.Model;
-import org.javalite.activejdbc.annotations.Table;
 
-@Table("statuses")
 public class Status extends Model {}
