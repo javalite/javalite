08b60d9456ef9f3c9ba85333a573f972a1f11dc1
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2022-01-12 22:41:02 -0600

    #1074 Implement tests against local docker containers with all supported databases - added Oracle, but do not yet know how to connect to it. Also, ToJsonSpec.shouldReturnSecondsInDateTime fails on MSSQL
    (cherry picked from commit c9ae55e3d8117d8d55272a004c0d5435f160bc91)

--- activejdbc/docker/mariadb/start.sh
+++ activejdbc/docker/mariadb/start.sh
@@ -1,9 +1,2 @@
 
-docker rm mariadb
-docker run  \
---name mariadb \
---env-file=env.mariadb \
---volume=$PWD/data:/var/lib/mysql \
---volume=$PWD/data:/etc/mysql/conf.d \
---volume=$PWD/data:/var/log/mysql \
---volume=/tmp:/tmp -p 127.0.0.1:3307:3306 mariadb:latest
+docker start javalite-mariadb
--- activejdbc/docker/mysql/install-run.sh
+++ activejdbc/docker/mysql/install-run.sh
@@ -1,4 +1,4 @@
 
 export MYSQL_DATABASE=activejdbc
-docker run --name  javalite-mysql -p 3307:3306 -e MYSQL_ROOT_PASSWORD=p@ssw0rd  -e MYSQL_DATABASE=activejdbc -d mysql:8.0.27
+docker run --name  javalite-mysql -p 3308:3306 -e MYSQL_ROOT_PASSWORD=p@ssw0rd  -e MYSQL_DATABASE=activejdbc -d mysql:8.0.27
 
