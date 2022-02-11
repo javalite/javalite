0d27f480c41be695d4d2277e109f0097b8a5beca
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2022-02-11 12:13:14 -0600

    fixing integration test dependencies versions

--- activejdbc/instrumentfast.sh
+++ activejdbc/instrumentfast.sh
@@ -2,7 +2,7 @@
 
 ## This script will instrument fast without Maven
 ## Adjust versions on the classpath if upgrading!
-export JAVALITE=3.0-j11-SNAPSHOT
+export JAVALITE=3.1-j11-SNAPSHOT
 
 export CLASSPATH=~/.m2/repository/org/javalite/activejdbc-instrumentation/$JAVALITE/activejdbc-instrumentation-$JAVALITE.jar
 export CLASSPATH=${CLASSPATH}:~/.m2/repository/org/javassist/javassist/3.18.2-GA/javassist-3.18.2-GA.jar
