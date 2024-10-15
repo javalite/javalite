6b52b44ee273110e4390dbb4dd93eff9dc329793
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2024-10-15 17:15:27 -0500

    #1323 - Implement ability for embedded  Tomcat and a single jar app
     - initial commit - seems there is a Servlet conflict on classpath. I think we need to move to Jakarta and the newest Servlet API, as well as Tomcat version before doing this.

---
+++ embedded-tomcat/src/test/java/app/config/AppBootstrap.java
@@ -0,0 +1,11 @@
+package app.config;
+
+import org.javalite.activeweb.AppContext;
+import org.javalite.activeweb.Bootstrap;
+
+public class AppBootstrap extends Bootstrap {
+
+    @Override
+    public void init(AppContext appContext) {
+    }
+}
---
+++ embedded-tomcat/src/test/java/app/config/AppControllerConfig.java
@@ -0,0 +1,10 @@
+package app.config;
+
+import org.javalite.activeweb.AbstractControllerConfig;
+import org.javalite.activeweb.AppContext;
+
+public class AppControllerConfig extends AbstractControllerConfig {
+    @Override
+    public void init(AppContext appContext) {
+    }
+}
