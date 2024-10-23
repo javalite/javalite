aff7c9d50f6dc2e056bd182cc2b15b543950ec8d
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2024-10-23 13:22:01 -0500

    #1323 - Implement ability for embedded Tomcat and a single jar app

--- embedded-tomcat-integration-test/src/main/java/app/config/DbConfig.java
+++ embedded-tomcat-integration-test/src/main/java/app/config/DbConfig.java
@@ -4,12 +4,12 @@ package app.config;
 import org.javalite.activeweb.AbstractDBConfig;
 import org.javalite.activeweb.AppContext;
 
+import static org.javalite.app_config.AppConfig.p;
+
 public class DbConfig extends AbstractDBConfig {
 
     @Override
     public void init(AppContext appContext) {
-        environment("development").jndi("java:comp/env/jdbc/myDatabasePool");
-//        environment("development", true).jndi("java:comp/env/jdbc/myDataSource"); -- working
-
+        environment("development").jndi("java:comp/env/" + p("embedded.tomcat.pool.name"));
     }
 }
